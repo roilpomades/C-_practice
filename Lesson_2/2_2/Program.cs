3db99000a27d409c59a98c59db8e382e35faea03
roilpomades <140910317+roilpomades@users.noreply.github.com>
2024-05-15 18:10:13 +0300

    Lesson 2:Task2

---
+++ Lesson_2/2_2/Program.cs
@@ -0,0 +1,10 @@
+﻿
+Console.WriteLine("Enter three-digit number:");
+//int a = int.Parse(Console.ReadLine()!);
+
+int a = int.Parse(Console.ReadLine()!);
+
+int firstDigit = a / 10 % 10;
+int thirdDigit = a % 10;
+
+Console.WriteLine(Math.Pow(firstDigit,thirdDigit));
