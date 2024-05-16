f7422606c14e54972406d6f6812894544b83ba94
roilpomades <140910317+roilpomades@users.noreply.github.com>
2024-05-16 16:32:07 +0300

    Lesson 2:Task3

--- Lesson_2/2_1/Program.cs
+++ Lesson_2/2_1/Program.cs
@@ -1,4 +1,4 @@
 ﻿Console.WriteLine("Enter a number");
 int a = int.Parse(Console.ReadLine()!);
-Console.WriteLine(a / 100 * 10 + a % 10);
+Console.WriteLine(a / 100 * 10 + a % 10); //Отделяет вторую цифру числа и третью цифру числа
 
